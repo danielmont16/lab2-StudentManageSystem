f766e68013d8d4e6073b373daa3be62c3fc3e5f9
daniel montenegro <dfelipemont@gmail.com>
2024-01-25 18:43:33 -0500

    adding try and catch

--- src/main/java/com/example/lab2studentmanagesystem/StudentController.java
+++ src/main/java/com/example/lab2studentmanagesystem/StudentController.java
@@ -4,9 +4,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-
-import java.util.Objects;
 
 public class StudentController {
     @FXML
